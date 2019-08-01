1ab85823dbe7113fcfafd0ed69dec6231f8ca8cc
netnr <netnr@netnr.com>
2019-08-01 17:19:11 +0800

    update comment

--- src/Netnr.ClassLibrary/Netnr.Test/Program.cs
+++ src/Netnr.ClassLibrary/Netnr.Test/Program.cs
@@ -6,12 +6,6 @@ namespace Netnr.Test
     {
         static void Main(string[] args)
         {
-            var hwr = Core.HttpTo.HWRequest("");
-
-            System.Net.HttpWebResponse v1 = Core.HttpTo.UrlResponse(hwr);
-
-            var v2 = v1;
-
             Console.WriteLine("Hello World!");
         }
     }
